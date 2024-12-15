8536071266927e871cf98663b09f1ebd35861833
NicatHuseyn <nicat5075@gmail.com>
2024-12-15 08:39:55 -0800

    DebuggerDisplay Attribute Tutorial

---
+++ DebuggerDisplayAttribute/DebuggerDisplayAttribute/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Diagnostics;
+
+Person person = new Person() 
+{ 
+    Name = "Nijat",
+    Age = 22,
+    City = "Baku",
+    Region = "Tovuz" 
+};
+
+var p = person;
+
+Console.WriteLine(p);
+
+
+[DebuggerDisplay("Name={Name} | Age={Age} | City={City} | Region={Region}")]
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string City { get; set; }
+    public string Region { get; set; }
+}
+
